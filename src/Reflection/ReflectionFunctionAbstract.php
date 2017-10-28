9470f8017a58ee803e1aba5472ea04b68ed1a933
TomasVotruba <tomas.vot@gmail.com>
2017-10-28 15:51:43 +0200

    fix toString()

--- src/Reflection/ReflectionFunctionAbstract.php
+++ src/Reflection/ReflectionFunctionAbstract.php
@@ -143,7 +143,7 @@ abstract class ReflectionFunctionAbstract implements CoreReflector
             return self::CLOSURE_NAME;
         }
 
-        return $this->node->name;
+        return $this->node->name->toString();
     }
 
     /**
